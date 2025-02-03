24a6242bd7b48c22ae7edae0da2fc61e91a4cc7d
tamld <ductam1828@gmail.com>
2025-02-03 16:54:50 +0700

    Force CRLF for helpdesk-tools.cmd

--- helpdesk-tools.cmd
+++ helpdesk-tools.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-
+:: Force CR+LF encoding
 :: ============================================================
 :: Helpdesk Tools
 :: ============================================================
